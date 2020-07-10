8777b9063a6047cf31efff4d8d8461ded4a9af02
Premal Singh <premals98@gmail.com>
2020-07-10 16:24:23 +0530

    Created UI for Adding product

---
+++ app/src/main/java/com/example/nmc/AddNewPdt_Activity.java
@@ -0,0 +1,14 @@
+package com.example.nmc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewPdt_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_pdt_);
+    }
+}
--- app/src/main/java/com/example/nmc/MainActivity.java
+++ app/src/main/java/com/example/nmc/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.nmc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ListIterator;
@@ -15,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button add_new_pdt_btn = (Button) findViewById(R.id.add_new_pdt_btn);
+        add_new_pdt_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AddNewPdt_Activity.class));
+            }
+        });
     }
 }
